b4b20126d77135266065ab228ef0917ea5521d80
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-08-26 13:05:43 -0400

    More notes to Angular

--- src/app/angular-notes/angular-notes.component.ts
+++ src/app/angular-notes/angular-notes.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class AngularNotesComponent implements OnInit {
   theVariable :string =  "" ;
   theBoolVar: boolean = true;
+  theColor: string = "aqua";
 
   ngOnInit(){
     window.scroll(0,0);
@@ -23,5 +24,31 @@ export class AngularNotesComponent implements OnInit {
 
   onSetVariableToHello() {
     this.theVariable = "hello";
+  }
+
+  onRandomColor(){   
+    //todo random function to use in common
+    //between 2 numbers Math.floor(Math.random() * (max - min + 1)) + min;
+    // Between 0 and max Math.floor(Math.random() * (max + 1));
+    // Between 1 and max Math.floor(Math.random() * max) + 1;     
+
+    switch(Math.floor(Math.random() * 6) + 1){
+      case 1: this.theColor= "aqua";
+              break;
+      case 2: this.theColor="red";
+              break;
+      case 3: this.theColor="pink";
+              break;
+      case 4: this.theColor="yellow";
+              break;
+      case 5: this.theColor="green";
+              break;
+      case 6: this.theColor="gray";
+              break;
+      default: this.theColor = "aqua";
+              break;
+
+    }
+      
   }
 }
