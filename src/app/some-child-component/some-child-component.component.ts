0d0b339af28b082e71e87ebc5a796caa5ba16c40
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-08-29 13:42:51 -0400

    Adding ContentChild notes

--- src/app/some-child-component/some-child-component.component.ts
+++ src/app/some-child-component/some-child-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output,EventEmitter } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input, Output,EventEmitter, ContentChild,ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'some-child-component',
@@ -6,17 +7,28 @@ import { Component, Input, Output,EventEmitter } from '@angular/core';
   styleUrls: ['./some-child-component.component.css']
 })
 
-export class SomeChildComponentComponent {
+export class SomeChildComponentComponent implements AfterContentInit {
   @Input() myColor :string="";
   @Input('myTextAlias') myText :string="";
 
   @Output() eventOnChildComponentWithKnownObjReturn = new EventEmitter<{theName:string, theText:string}>();
   @Output() eventOnChildComponentWithEventReturn = new EventEmitter<Event>();
 
+  @ContentChild("header1",{static:true}) header1vble: ElementRef={} as ElementRef;
+  @ContentChild("header2",{static:true}) header2vble: ElementRef={} as ElementRef;
+  @ContentChild("span1",{static:true, read:NgStyle}) spanvble: NgStyle={} as NgStyle;
+  
+
   launchEvents(event:Event){
 
     this.eventOnChildComponentWithKnownObjReturn.emit({theName:"Child Name", theText:"Child Text Hello"});
     this.eventOnChildComponentWithEventReturn.emit(event);
+  }
+
+  ngAfterContentInit(){    
+    console.log(this.header1vble);
+    console.log(this.header2vble);
+    console.log(this.spanvble);
   }
 
 }
