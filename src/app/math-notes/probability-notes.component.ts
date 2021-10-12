e365241288ef734cc829085f476edbd9f16f4069
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-10-12 10:53:29 -0400

    fix name error

--- src/app/math-notes/probability-notes.component.ts
+++ src/app/math-notes/probability-notes.component.ts
@@ -6,7 +6,7 @@ import { getRandom1ToN } from '../../assets/sharedFunctions';
 @Component({
   selector: 'probability-notes',
   templateUrl: './probability-notes.component.html',
-  styleUrls: ['.math-notes.component.css']
+  styleUrls: ['./math-notes.component.css']
 })
 
 export class ProbabilityNotesComponent implements OnInit {
