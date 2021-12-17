2d323e71166cc099b6979825a06a9549c8e7e28b
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2021-12-17 14:20:01 -0500

    Add href to just between page sections

--- src/app/shared/forexamples.service.ts
+++ src/app/shared/forexamples.service.ts
@@ -1,16 +1,12 @@
 import { Injectable , EventEmitter} from "@angular/core"
 
 
-
 export class MathServices{
 
     getRandom1ToN(n:number) {
         return Math.floor(Math.random() * n) + 1
     }            
 }
-
-
-
 
 Injectable({providedIn: 'root'})
 export class StringServices{
