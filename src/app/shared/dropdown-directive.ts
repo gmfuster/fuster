6d5ed16d91e1bc4d40d9cabe79afbef4f418fbe6
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2022-01-10 00:12:08 -0500

    change the drop down directive

--- src/app/shared/dropdown-directive.ts
+++ src/app/shared/dropdown-directive.ts
@@ -4,8 +4,20 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
     selector:'[appDropDown]'
 })
 export class DropdownDirective {
-    @HostBinding('class.open') isOpen = false;
-    @HostListener('click') toggleOpen(){
-        this.isOpen = !this.isOpen;
+    @HostBinding('class.open') isOpen = false;   
+    isClicked = false;   
+    @HostListener('click') toggleOpen(){        
+        this.isClicked = true;
+        //this.isOpen = !this.isOpen;       
+    }
+    @HostListener('document:click') docClicked(){        
+        if (this.isClicked)
+        {
+            this.isOpen = !this.isOpen; 
+            this.isClicked = false;
+        }else{
+            this.isOpen = false;            
+        }
+        
     }
 }
