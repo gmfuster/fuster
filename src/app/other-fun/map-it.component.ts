fef15ed8f398042f0db67a49edadbd22b68417e0
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2022-03-26 14:53:35 -0400

    just a few more maps

--- src/app/other-fun/map-it.component.ts
+++ src/app/other-fun/map-it.component.ts
@@ -9,10 +9,14 @@ import { Component , OnInit} from '@angular/core';
 export class MapItComponent implements OnInit{   
 
   mapSVGPath:string = "\\assets\\images\\Other\\";  
-  mapSVG: string = this.mapSVGPath +"worldmap.svg"
-  listOfEvents: {title:string, map:string}[] = [{title:"", map:"worldmap.svg"},
-    {title:"Iraq War 2003", map:"worldmap_iraq.svg"},
-    {title:"Afghanistan War 2001", map:"worldmap_afghanistan.svg"} ]  
+  mapSVG: string = this.mapSVGPath +"worldmap.svg";  
+  listOfEvents: {title:string, map:string, add:string,}[] = [
+    {title:"", map:"worldmap.svg",add:""},
+    {title:"Participation Iraq War 2003", map:"worldmap_iraq.svg", add:""},
+    {title:"Participation Afghanistan War 2001", map:"worldmap_afghanistan.svg",add:""},
+    {title:"Top 10 polluters per capita 2021", map:"worldmap_top_polluters_21.svg",add:""},
+    /*{title:"Bombed by US 1991-2021", map:"worldmap_bomb_us.svg",add:"I tried to find the data on Russia, China but not much on it"}] -- add back when available for other too*/
+  ]
   selectedEvent :string = "";
   ngOnInit(){
     window.scroll(0,0);     
@@ -20,6 +24,6 @@ export class MapItComponent implements OnInit{
   }
 
   onEventChanged(e:any){    
-    this.mapSVG = this.mapSVGPath + e.target.value;
+    this.mapSVG = this.mapSVGPath + e.target.value;    
   }
 }  
