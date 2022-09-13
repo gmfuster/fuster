730e314218adefad2c088a729ab2e6a3e470e985
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2022-09-13 09:25:57 -0400

    for (

--- src/app/angular-notes/angular-notes-http.component.ts
+++ src/app/angular-notes/angular-notes-http.component.ts
@@ -1,13 +1,19 @@
 //import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
+
+interface ForSend{
+  name: string
+}
 
 @Component({
   selector: 'angular-notes-http',
   templateUrl: './angular-notes-http.component.html',
   styleUrls: ['./angular-notes.component.css']
 })
+
 
 export class AngularNotesHttpComponent implements OnInit {
 
@@ -19,10 +25,33 @@ export class AngularNotesHttpComponent implements OnInit {
   }
 
   onSendAPostCliked(){    
-    var test = { 
-      name:"Michael", 
-      lastname:"Jackson"
-    }
     
   }
+  onSendAGetCliked(){    
+    
+    this.http.get<ForSend[]>("http://localhost:3000/sendAGet", 
+      {
+        headers :new HttpHeaders( { 'content-type': 'application/json'}) ,
+        observe : 'body'
+      }).pipe(
+      map(  
+        (value ) => 
+          {                       
+            for (var v = 0; v < 4; v++ )
+            {            
+              //value[v].name += " changed it!";
+            }               
+            return value;      
+          }        
+        )
+      ).subscribe( responseData => 
+      {        
+        console.log(responseData);
+      },
+      error => {
+        alert("Oh my something happened");
+      });
+
+  }    
+
 }
