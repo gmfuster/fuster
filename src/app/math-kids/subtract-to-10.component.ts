057fcdedf671f4e16f7cf34dae5a57292877b483
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2022-10-19 20:50:17 -0400

    fix the make 10 to subtract

--- src/app/math-kids/subtract-to-10.component.ts
+++ src/app/math-kids/subtract-to-10.component.ts
@@ -26,17 +26,76 @@ export class SubtractTo10Component implements OnInit, OnDestroy  {
    
   }  
   onShowHow(){
+    
+  }
+  calculate(){
+    //get last digit of from number
+    var fromNumString = this.subtractFromNum.toString();
+    var lastDigit = fromNumString.substr(fromNumString.length - 1);
+    this.secondDigitFromNumber = parseInt(lastDigit);
+    var firstDigit = fromNumString.substr(0,1);
+    this.firstDigitFromNumber = parseInt(firstDigit);
+    this.subtractNumMinusSecondDigit = this.subtractNum - parseInt(lastDigit);
+    this.subtractionRes = this.subtractFromNum - this.subtractNum;
+    
+    this.renderer.addClass(this.mySolution.nativeElement, 'visible');  
+    this.renderer.removeClass(this.mySolution.nativeElement, 'invisible');       
+  }
 
+  isChangedAllowed(subtractFromNum:number, subtractNum:number){
+    //if the second digit of subtractFromNumber is bigger than the subtractFrom number, don't do it.
+    //not used yet
+    return true;
+    var lastDigit = subtractFromNum.toString().substr(subtractFromNum.toString().length - 1); 
+    var lastDigitNum = parseInt(lastDigit);
+    if (lastDigitNum > subtractNum){
+      return false;
+    }
+    return true;
   }
-  numbersChanged(event:Event){
-    console.log(this.subtractFromNum);  
-    //var charCode = (event.which) ? event.which : event.keyCode;
-  }
-  numbersChangedk(event:KeyboardEvent){
-    if (event.key === "."){
+
+  //todo the prevent default is not working right, doesn't matter now because it's always OK
+  //also, not doing the calcs unless the button is clicked.
+  numbersChanged(event:Event, fromWhere:string){  
+    this.renderer.addClass(this.mySolution.nativeElement, 'invisible'); 
+    this.renderer.removeClass(this.mySolution.nativeElement, 'visible');   
+    /*
+    let subtractFromNum, subtractNum;
+    if (fromWhere === "subtractFromNum"){
+      subtractFromNum = parseInt((event.target as HTMLInputElement).value);
+      subtractNum = this.subtractNum;      
+    }else{
+      subtractNum = parseInt((event.target as HTMLInputElement).value);
+      subtractFromNum = this.subtractFromNum;
+    }    
+    if (!this.isChangedAllowed(subtractFromNum,subtractNum)) {
       event.preventDefault();
     }
-    
+    else{
+      this.calculate();    
+    }
+    */
+  }
+  //todo the prevent default is not working right, doesn't matter now because it's always OK
+  //also, not doing the calcs unless the button is clicked.
+  numbersChangedk(event:KeyboardEvent, fromWhere:string){
+    this.renderer.addClass(this.mySolution.nativeElement, 'invisible'); 
+    this.renderer.removeClass(this.mySolution.nativeElement, 'visible');   
+    /*
+    let subtractFromNum, subtractNum;
+    if (fromWhere === "subtractFromNum"){
+      subtractFromNum = parseInt((event.target as HTMLInputElement).value);
+      subtractNum = this.subtractNum;      
+    }else{
+      subtractNum = parseInt((event.target as HTMLInputElement).value);
+      subtractFromNum = this.subtractFromNum;
+    }
+    if (event.key === "." || !this.isChangedAllowed(subtractFromNum,subtractNum )){
+      event.preventDefault();
+    }else{
+      this.calculate();
+    }    
+    */
   }
   ngOnDestroy(): void {
     this.renderer.removeClass(document.body, 'mainBodyScrollingBlocked');
