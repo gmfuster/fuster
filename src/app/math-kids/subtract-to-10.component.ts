eceeec27c5f6ff6c2fdb08f55d32ee47d8d75605
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2022-10-21 21:04:51 -0400

    fixes and changes to subtract 10 to count

--- src/app/math-kids/subtract-to-10.component.ts
+++ src/app/math-kids/subtract-to-10.component.ts
@@ -1,3 +1,4 @@
+import { keyframes } from '@angular/animations';
 import { Component, ElementRef, OnInit, ViewChild, OnDestroy, Renderer2 } from '@angular/core';
 
 @Component({
@@ -14,6 +15,8 @@ export class SubtractTo10Component implements OnInit, OnDestroy  {
   secondDigitFromNumber:number=1;
   subtractionRes:number = 10;
   subtractNumMinusSecondDigit:number = 0;
+  subtractNumMinusSecondDigitString:string = "0";
+  plusOrMinus:string = "-";
   
 
   constructor (private renderer: Renderer2){      
@@ -35,32 +38,39 @@ export class SubtractTo10Component implements OnInit, OnDestroy  {
     this.secondDigitFromNumber = parseInt(lastDigit);
     var firstDigit = fromNumString.substr(0,1);
     this.firstDigitFromNumber = parseInt(firstDigit);
+    //if this number is negative it gets more complicated so don't allow for this for now
     this.subtractNumMinusSecondDigit = this.subtractNum - parseInt(lastDigit);
+    if (this.subtractNumMinusSecondDigit < 0 ){
+      this.subtractNumMinusSecondDigitString = (this.subtractNumMinusSecondDigit * (-1)).toString();
+      this.plusOrMinus = "+"
+    }
+    else{
+      this.subtractNumMinusSecondDigitString = (this.subtractNumMinusSecondDigit).toString();
+      this.plusOrMinus = "-";
+    }
     this.subtractionRes = this.subtractFromNum - this.subtractNum;
     
     this.renderer.addClass(this.mySolution.nativeElement, 'visible');  
-    this.renderer.removeClass(this.mySolution.nativeElement, 'invisible');       
+    this.renderer.removeClass(this.mySolution.nativeElement, 'invisible');   
+        
   }
 
   isChangedAllowed(subtractFromNum:number, subtractNum:number){
     //if the second digit of subtractFromNumber is bigger than the subtractFrom number, don't do it.
-    //not used yet
-    return true;
-    var lastDigit = subtractFromNum.toString().substr(subtractFromNum.toString().length - 1); 
-    var lastDigitNum = parseInt(lastDigit);
-    if (lastDigitNum > subtractNum){
+    //not used yet    
+    var lastDigitOfSubtractFrom = subtractFromNum.toString().substr(subtractFromNum.toString().length - 1); 
+    var lastDigitOfSubtractFromNum = parseInt(lastDigitOfSubtractFrom);
+    if (lastDigitOfSubtractFromNum > subtractNum){
       return false;
     }
     return true;
   }
-
-  //todo the prevent default is not working right, doesn't matter now because it's always OK
-  //also, not doing the calcs unless the button is clicked.
+  
   numbersChanged(event:Event, fromWhere:string){  
     this.renderer.addClass(this.mySolution.nativeElement, 'invisible'); 
     this.renderer.removeClass(this.mySolution.nativeElement, 'visible');   
-    /*
-    let subtractFromNum, subtractNum;
+    
+    /*let subtractFromNum, subtractNum;
     if (fromWhere === "subtractFromNum"){
       subtractFromNum = parseInt((event.target as HTMLInputElement).value);
       subtractNum = this.subtractNum;      
@@ -71,7 +81,11 @@ export class SubtractTo10Component implements OnInit, OnDestroy  {
     if (!this.isChangedAllowed(subtractFromNum,subtractNum)) {
       event.preventDefault();
     }
-    else{
+    var allowed = this.isChangedAllowed(subtractFromNum,subtractNum );    
+    if (!allowed){
+      event.preventDefault();
+    }
+    /*else{
       this.calculate();    
     }
     */
@@ -81,21 +95,29 @@ export class SubtractTo10Component implements OnInit, OnDestroy  {
   numbersChangedk(event:KeyboardEvent, fromWhere:string){
     this.renderer.addClass(this.mySolution.nativeElement, 'invisible'); 
     this.renderer.removeClass(this.mySolution.nativeElement, 'visible');   
-    /*
-    let subtractFromNum, subtractNum;
+    
+    /*let subtractFromNum, subtractNum;
     if (fromWhere === "subtractFromNum"){
-      subtractFromNum = parseInt((event.target as HTMLInputElement).value);
-      subtractNum = this.subtractNum;      
-    }else{
-      subtractNum = parseInt((event.target as HTMLInputElement).value);
-      subtractFromNum = this.subtractFromNum;
+      if (event.key ===  "Backspace" || event.key === "Delete")
+      {
+        subtractFromNum = parseInt( (event.target as HTMLInputElement).value);
+      }else{
+        subtractFromNum = parseInt( (event.target as HTMLInputElement).value + event.key);
+      }     
+        subtractNum = this.subtractNum;      
+    }else{            
+        subtractNum = parseInt( (event.target as HTMLInputElement).value + event.key);           
+        subtractFromNum = this.subtractFromNum;
     }
-    if (event.key === "." || !this.isChangedAllowed(subtractFromNum,subtractNum )){
+
+    var allowed = this.isChangedAllowed(subtractFromNum,subtractNum );    */
+    if (event.key === "."){
       event.preventDefault();
-    }else{
+    }
+    /*else{
       this.calculate();
-    }    
-    */
+    } */   
+    
   }
   ngOnDestroy(): void {
     this.renderer.removeClass(document.body, 'mainBodyScrollingBlocked');
