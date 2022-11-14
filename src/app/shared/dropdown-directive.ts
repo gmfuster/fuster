a93c5262b04f36b721e74baabfc7c83b1ab40547
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2022-11-14 15:56:57 -0500

    Close drop down on mouseleave

--- src/app/shared/dropdown-directive.ts
+++ src/app/shared/dropdown-directive.ts
@@ -5,6 +5,7 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
 })
 export class DropdownDirective {
     @HostBinding('class.open') isOpen = false;   
+    @HostListener('mouseleave') mouseE(eventData:Event) { this.isOpen = false; }
     isClicked = false;   
     @HostListener('click') toggleOpen(){        
         this.isClicked = true;
