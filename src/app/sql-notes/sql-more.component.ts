941ab0e03343d172e5e12b35d0718d5037b7b8cb
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2023-02-09 18:14:10 -0500

    add more sql section

---
+++ src/app/sql-notes/sql-more.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit,ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
+import {HeadingSubTopicDir} from '../shared/directives';
+import {TopLinksComponent} from  '../shared/top-links.component';
+import {CommonFuncs} from '../shared/commonFuncs.service'
+
+@Component({
+  selector: 'sql-more',
+  templateUrl: './sql-more.component.html',
+  styleUrls: [], //share the css file  
+  providers: [CommonFuncs]
+})
+
+export class SQLMoreComponent implements OnInit {
+@ViewChildren(HeadingSubTopicDir, { read: ElementRef }) headings!:QueryList<any>;
+myHeadings : string[] = [];
+myName : string = "sql";
+@ViewChild(TopLinksComponent, {static : true}) child! : TopLinksComponent  ;   
+
+  constructor(private commonFuncs:CommonFuncs){}
+
+    ngOnInit(){
+        window.scroll(0,0);
+    }    
+    ngAfterViewInit(){       
+     this.myHeadings = this.commonFuncs.getIdsFromHeadingSubTopicElements(this.headings);           
+     this.child.getChangesFromParent(this.myHeadings,this.myName )    
+    }  
+        
+}
--- src/app/sql/sql.module.ts
+++ src/app/sql/sql.module.ts
@@ -5,13 +5,15 @@ import { SQLRoutingModule } from './sql-routing.module';
 import { SQLComponent } from './sql.component';
 
 import { SQLBasicsComponent } from '../sql-notes/sql-basics.component';
+import { SQLMoreComponent } from '../sql-notes/sql-more.component';
 
 import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
     SQLComponent,
-    SQLBasicsComponent
+    SQLBasicsComponent,
+    SQLMoreComponent
   ],
   imports: [
     CommonModule,
