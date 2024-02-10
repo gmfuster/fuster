7bc3638117c0366bd6f1b92ba185d7725e37b75c
gemmaf98 <44875585+gemmaf98@users.noreply.github.com>
2024-02-10 13:14:31 -0500

    Start the code for the 2 step problems

--- src/app/math-kids3/pages/2StepProblem.component.ts
+++ src/app/math-kids3/pages/2StepProblem.component.ts
@@ -9,7 +9,15 @@ import { CommonFuncs } from '../../shared/commonFuncs.service';
 })
 
 export class TwoStepProblemComponent implements OnInit, OnDestroy  {
-  
+
+  containers: string[] = ["boxes", "pools", "hats", "bags", "sharks", "cars", "houses", "baskets", "paintings", "tables"];
+  containersSing: string[] = ["box", "pool", "hat", "bag", "shark", "car", "house", "basket", "painting", "table"];
+  smallitems: string[] = ["stones", "pins", "teeth", "goldfish", "pens", "figurines", "coins", "buttons", "pencils", "erasers"];
+  containersNumber: number = 0;
+  SmallItemsNumber: number = 0;
+  startWithContainer: boolean | null = null;
+  doPlus: boolean | null = null;
+  text:string = "";
   constructor (private renderer: Renderer2, private commonFuncs:CommonFuncs){      
     this.renderer.addClass(document.body, 'mainBodyScrollingBlocked');  
     this.renderer.removeClass(document.body, 'mainBodyScrolling');      
@@ -20,7 +28,32 @@ export class TwoStepProblemComponent implements OnInit, OnDestroy  {
   
   }  
 
-  
+  onGetProblemClick(){
+    this.startWithContainer = (this.commonFuncs.getRandom0ToN(2) == 1); //to get 0 or 1
+    this.doPlus = (this.commonFuncs.getRandom0ToN(1) == 1);
+
+    this.containersNumber = this.commonFuncs.getRandom1ToN(10);
+    this.SmallItemsNumber = this.containersNumber * this.commonFuncs.getRandom1ToN(4);  
+
+    var index1 = this.commonFuncs.getRandom0ToN(9);
+    var index2 = this.commonFuncs.getRandom0ToN(9);
+
+    if (this.startWithContainer){
+                
+      this.text = "We have " + this.containersNumber + " " + this.containers[index1] + " and each one has " + this.SmallItemsNumber + " " + this.smallitems[index2] + " in them. " ;
+      if (this.doPlus)
+      {
+        this.text += "Somebody gives us " + (this.SmallItemsNumber + 3) + " more.  How many " + this.smallitems[index2] + "do we have now?";
+      }
+      else{
+        this.text +=  "We give a friend " + (this.SmallItemsNumber - 1) + " . How many " + this.smallitems[index2] + " do we have now?";
+      }
+      
+    }else{
+      this.text = "We have " + this.SmallItemsNumber + " "  + this.smallitems[index2] + " but we need to split them in " + this.containersNumber + " " + this.containers[index1] + " .";      
+      this.text += " Somebody gives us " + (this.SmallItemsNumber - 2) +  " more to put in that one " + this.containersSing[index1] + " .How many " + this.smallitems[index2] + " do we have now in the " + this.containersSing[index1] + "?";      
+    }
+  }
 
   reset(){
   
